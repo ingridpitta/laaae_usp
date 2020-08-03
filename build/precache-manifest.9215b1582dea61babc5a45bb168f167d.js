fcf5a665acfc984401b28116d4421710cffe8b8b
ingridpitta <ingridpitta056@gmail.com>
2020-08-03 15:14:30 -0300

    build

---
+++ build/precache-manifest.9215b1582dea61babc5a45bb168f167d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c142db5ecde0ae77fd63c72d1d10c7",
+    "url": "/wp-content/themes/laaae_usp/index.php"
+  },
+  {
+    "revision": "0b3b3e5140789e23cefb",
+    "url": "/wp-content/themes/laaae_usp/static/css/2.chunk.css"
+  },
+  {
+    "revision": "6d6a5c1959ca027e607f",
+    "url": "/wp-content/themes/laaae_usp/static/css/main.chunk.css"
+  },
+  {
+    "revision": "0b3b3e5140789e23cefb",
+    "url": "/wp-content/themes/laaae_usp/static/js/2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wp-content/themes/laaae_usp/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9449def0fef9ac60ac76",
+    "url": "/wp-content/themes/laaae_usp/static/js/bundle.js"
+  },
+  {
+    "revision": "6d6a5c1959ca027e607f",
+    "url": "/wp-content/themes/laaae_usp/static/js/main.chunk.js"
+  },
+  {
+    "revision": "91600034e9f50cb8022cd609c8c53383",
+    "url": "/wp-content/themes/laaae_usp/static/media/arrow_right.91600034.svg"
+  },
+  {
+    "revision": "0a8849d118b4b0295a478e04860e1562",
+    "url": "/wp-content/themes/laaae_usp/static/media/fb_icon.0a8849d1.svg"
+  },
+  {
+    "revision": "907c924b43a5f8d3b91b702a852e3cb9",
+    "url": "/wp-content/themes/laaae_usp/static/media/insta_icon.907c924b.svg"
+  },
+  {
+    "revision": "c3c7113d8cb40127fca86f7788e7d793",
+    "url": "/wp-content/themes/laaae_usp/static/media/main_picture.c3c7113d.png"
+  }
+]);
